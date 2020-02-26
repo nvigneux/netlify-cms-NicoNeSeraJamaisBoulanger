967060ecba86d293d9c91e97d596e7ad90e9aa8d
VIGNEUX Nicolas <vigneuxnicolas@gmail.com>
2020-02-26 19:57:44 +0100

    [*] opacity scroll

--- src/pages/contact/file-upload.js
+++ src/pages/contact/file-upload.js
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import PropTypes from "prop-types";
 import "./playground.scss";
 
+export const Actions = () => {
+  const [lastYPos, setLastYPos] = useState(0);
+  const [shouldShowActions, setShouldShowActions] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      const yPos = window.scrollY;
+      const isScrollingUp = yPos < lastYPos;
+
+      setShouldShowActions(isScrollingUp);
+      setLastYPos(yPos);
+    }
+
+    window.addEventListener("scroll", handleScroll, false);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll, false);
+    };
+  }, [lastYPos]);
+  return (
+    <motion.div
+      className="actions"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: shouldShowActions ? 1 : 0 }}
+      transition={{ opacity: { duration: 0.2 } }}
+    >
+      <button>
+        <span className="fas fa-share-square">a</span>
+      </button>
+      <button>
+        <span className="fas fa-share-square">b</span>
+      </button>
+      <button>
+        <span className="fas fa-share-square">c</span>
+      </button>
+    </motion.div>
+  );
+};
+
 export const Playground = () => {
   return (
-    <div className="container">
+    <div className="container-playground">
       <article className="info">
         <header className="header">
           <span className="subtitle">React Animation</span>
@@ -18,7 +58,7 @@ export const Playground = () => {
           </p>
         </section>
       </article>
-      {/* <Actions /> */}
+      <Actions />
     </div>
   );
 };
